fe51cb6a618c396a8857a5139e5708b3d77305e1
KAzumix0327 <yamauchi138@icloud.com>
2022-10-19 00:56:47 +0900

    小型昇竜拳？？？

--- Syoryuken/ViewController.swift
+++ Syoryuken/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var ImageView: UIImageView!
+    //キャラ設置
+    var imageArrayAttack : Array<UIImage> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let image = UIImage(named: "attack1") //画像を変数に読み込む
+        ImageView.image = image // imageViewに読み込んだ画像をセット
+        
+        // 配列に格納
+        while let attackImage = UIImage(named: "attack\(imageArrayAttack.count+1)") {
+            imageArrayAttack.append(attackImage)
+            
+        }
     }
-
-
+    
+    @IBAction func button(_ sender: Any) {
+        // アニメーションの適用
+        ImageView.animationImages = imageArrayAttack
+        
+        ImageView.animationDuration = 2
+        // 1回繰り返し
+        ImageView.animationRepeatCount = 1
+        // アニメーションを開始
+        ImageView.startAnimating()
+    }
 }
 
